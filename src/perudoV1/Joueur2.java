b91553dc4cebda69f599ecc3512b34f9e1d2787b
Boz <lesaichotfrancois@gmail.com>
2017-01-12 16:17:01 +0100

    prob

---
+++ src/perudoV1/Joueur2.java
@@ -0,0 +1,52 @@
+package perudoV1;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+public class Joueur2 extends UnicastRemoteObject implements JoueurInt{
+	
+	
+	/**
+	 * 
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	//attributs
+	private int id_joueur;
+	private String pseudo;
+	private ArrayList<De> de_joueur;
+	int toto;
+	
+		
+	//constructeur
+	public Joueur2(int id_joueur, String pseudo, ArrayList<De> de_joueur) throws RemoteException {
+		super();
+		this.id_joueur = id_joueur;
+		this.pseudo = pseudo;
+		this.de_joueur = de_joueur;
+	}
+
+
+	//methodes	
+
+	public int getId_joueur() throws RemoteException  {
+
+		return id_joueur;
+	}
+
+	public String getPseudo() throws RemoteException{
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo)  throws RemoteException{
+		this.pseudo = pseudo;
+	}
+
+
+	public ArrayList<De> getDe_joueur() throws RemoteException{
+		return de_joueur;
+	}
+	
+
+}
