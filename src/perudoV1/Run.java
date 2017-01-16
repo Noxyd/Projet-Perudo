66c62d8edcaae0b72060239215db20420cb5a788
Samuel Garcia <samg15wr@gmail.com>
2017-01-16 09:03:19 +0100

    Modif du Run

--- src/perudoV1/Run.java
+++ src/perudoV1/Run.java
@@ -1,7 +1,6 @@
 package perudoV1;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+
 import java.rmi.Naming;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -11,16 +10,14 @@ public class Run {
 	public static void main(String[] args) throws Exception{
 		try {
 			LocateRegistry.createRegistry(1099);
-			String url ="//"+InetAddress.getLocalHost().getHostAddress()+"/Perudo";
 			
-			Partie partie1 = new Partie(0, null);
-			Naming.rebind(url, (Remote) partie1);
+			String url ="rmi://localhost/Perudo";
+			
+			Test_RMIImpl le_test = new Test_RMIImpl();
+			Naming.rebind(url, (Remote) le_test);
 			
 			
 		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
