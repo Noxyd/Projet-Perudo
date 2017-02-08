ee980d4f0c4f2fb70c63644a22a57ce9c79bc888
Samuel Garcia <samg15wr@gmail.com>
2017-02-08 17:56:14 +0100

    merge

--- src/perudoV1/Game.java
+++ src/perudoV1/Game.java
@@ -1,4 +1,4 @@
-package perudoV2;
+package perudoV1;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -6,6 +6,5 @@ import java.rmi.RemoteException;
 public interface Game extends Remote {
 	
 	public int connexion(Clients cli)throws RemoteException;
-	
-	public void ready()throws RemoteException;
+
 }
