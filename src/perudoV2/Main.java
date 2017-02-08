5f3ca8ea9f3393e42f117cf2e1ebc08175382bb0
Samuel Garcia <samg15wr@gmail.com>
2017-02-08 17:52:58 +0100

    Suppression des lignes commentées

--- src/perudoV2/Main.java
+++ src/perudoV2/Main.java
@@ -11,24 +11,6 @@ public class Main {
 		
 		try {
 			
-			/*GameImpl game = new GameImpl();
-			GameControl gc = new GameControl(game, 1);
-			Thread thGC = new Thread(gc);
-			
-			System.out.println("[SERVER ONLINE]");
-			
-			//Turn on the registry
-			LocateRegistry.createRegistry(1099);
-			
-			//Bind url with the shared game object
-			Naming.rebind("game-1", game);
-			
-			//This thread check if there is enough clients to begin
-			thGC.start();
-			
-			//The game wait for the notify of the GC
-			game.round();*/
-			
 			GameManagerImpl gm = new GameManagerImpl();
 			
 			//Turn on the registry
